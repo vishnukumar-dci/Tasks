ffda1f883ddc6d81079645f31b07ac77377f7d5e
vishnukumar-dci <vishnukumar.ganeshbabu@dotcominfoway.in>
2025-08-13 17:59:24 +0530

    coupon code integration

--- controller/stripe.js
+++ controller/stripe.js
@@ -4,60 +4,69 @@ const products = require('../products.json')
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
-const createCheckout = async(req,res) =>{
-    const productId = req.body.productId
-    const product = products.find(p => p.id === productId)
+const createCheckout = async (req, res) => {
+    const cart = JSON.parse(req.body.cartData)
+    const lineItems = []
 
-    if(!product)
-        return res.status(400).json({message:'Product Id is required'})
-
-    try {        
-        const session = await stripe.checkout.sessions.create({
-            payment_method_types:["card"],
-            mode:"payment",
-            line_items:[
-            {
-                price_data:{
-                    currency:"INR",
-                    product_data:{
-                        name:product.name
+    for (let id in cart) {
+        if (cart[id].qty > 0) {
+            const product = products.find(p => p.id === id)
+            if (product) {
+                lineItems.push({
+                    price_data: {
+                        currency: 'INR',
+                        product_data: {
+                            name: product.name
+                        },
+                        unit_amount: product.price * 100
                     },
-                    unit_amount:product.price*100,
-                },
-                quantity:1,
-            },
-            ],
-            success_url:"http://localhost:8088/payment/success?session_Id={CHECKOUT_SESSION_ID}",
-            cancel_url:"http://localhost:8088/payment/cancel",
+                    quantity: cart[id].qty
+                })
+            }
+        }
+    }
+
+    if (!lineItems.length) {
+        return res.status(400).send("Cart is empty");
+    }
+
+    try {
+        const session = await stripe.checkout.sessions.create({
+            payment_method_types: ["card"],
+            mode: "payment",
+            line_items: lineItems,
+            allow_promotion_codes:true,
+            success_url: "http://localhost:8088/payment/success?session_Id={CHECKOUT_SESSION_ID}",
+            cancel_url: "http://localhost:8088/payment/cancel",
         })
         console.log(session.url)
-        res.redirect(303,session.url)
+        res.redirect(303, session.url)
 
     } catch (error) {
         return res.status(500).send("Error creating checkout session")
     }
 }
 
-const paymentSuccess = async (req,res) =>{
-    const {session_Id} = req.query
+const paymentSuccess = async (req, res) => {
+    const { session_Id } = req.query
 
-    if(!session_Id){
-        return res.status(400).json({message:"Session ID missing"})
+    if (!session_Id) {
+        return res.status(400).json({ message: "Session ID missing" })
     }
 
-    try{
-      const session = await stripe.checkout.sessions.retrieve(session_Id)
-
-        res.render("success",{session})
+    try {
+        const session = await stripe.checkout.sessions.retrieve(session_Id)
+        console.log(session.amount_subtotal,session.amount_total)
+        res.render("success", { session })
     }
-    catch(error){
-      return res.status(500).json({message:'Error retrieving session'})
+    catch (error) {
+        return res.status(500).json({ message: 'Error retrieving session' })
     }
 }
 
-const paymentFail = async (req,res) =>{
+const paymentFail = async (req, res) => {
     res.render("cancel")
 }
 
 
-module.exports = {createCheckout,paymentSuccess,paymentFail}
+module.exports = { createCheckout, paymentSuccess, paymentFail }
